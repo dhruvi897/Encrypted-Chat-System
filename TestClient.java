70612b0b542f3a6e460f2e7210b930b8d0f9bc7c
Dhruvi Shah <61619085+dhruvi897@users.noreply.github.com>
2021-01-09 11:14:37 +0530

    Add files via upload

---
+++ TestClient.java
@@ -0,0 +1,54 @@
+import java.net.*;  
+import java.io.*;
+import java.util.Base64;
+class TestClient
+{  
+	public static void main(String args[]) throws Exception
+	{  
+		
+		try
+		{
+			RSA obj=new RSA();
+			obj.Keygenerate();
+			Socket s=new Socket("localhost",3333);  
+			DataInputStream din=new DataInputStream(s.getInputStream());  
+			DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+			  
+			String pk1="",str="",str2=""; 
+			String pubkey=obj.getpubkey();
+        	String privkey=obj.getprivkey();
+			dout.writeUTF(pubkey);
+			pk1=din.readUTF();
+			System.out.println("Eve's public key is: "+pk1);
+			while(true)
+			{  
+				int flag=0;
+				try
+				{
+					str=br.readLine(); 
+					String encryptedString = Base64.getEncoder().encodeToString(obj.encrypt(str, pk1)); 
+					dout.writeUTF(encryptedString);  
+					dout.flush();  
+					str2=din.readUTF();
+					System.out.println(str2);  
+					String msg=obj.decrypt(str2,privkey);
+					System.out.println("Eve says: "+msg); 
+					if(msg.equals("stop"))
+						flag=1; 
+				}
+				catch(Exception e){}
+				if(flag==1)
+					break;
+			}  
+			dout.flush();  
+			din.close();
+			dout.close();  
+			s.close(); 
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		} 
+	}
+}  
---
+++ Testserver.java
@@ -0,0 +1,103 @@
+import java.net.*;  
+import java.io.*; 
+import java.util.Base64;
+
+class Testserver
+{  
+    //public RSA obj;
+    public static void main(String args[])throws Exception
+    {  
+        ServerSocket ss=new ServerSocket(3333);
+        Socket s=null;
+        int cno=0;  
+        while(true)
+        {
+            try{    
+                s=ss.accept();
+                //cno++;  
+                System.out.println("Adam wants to send you a message : " + s);
+                DataInputStream din=new DataInputStream(s.getInputStream());  
+                DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+                //System.out.println("Assigning new thread for this"); 
+                Thread t=new ClientHandler(s,din,dout,cno);
+                t.start();
+            }
+            catch(Exception e)
+            {
+                s.close();
+                e.printStackTrace();
+            }
+        }
+         
+       // ss.close();  
+    }
+} 
+
+class ClientHandler extends Thread
+{
+    final DataInputStream din; 
+    final DataOutputStream dout; 
+    final int cnt;
+    final Socket s;
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    public ClientHandler(Socket s, DataInputStream din, DataOutputStream dout,int cnt)
+    {
+        this.s=s;
+        this.din=din;
+        this.dout=dout;
+        this.cnt=cnt;
+    }
+
+    @Override
+    public void run()
+    {
+        RSA obj=new RSA();
+        obj.Keygenerate();
+        String pubkey=obj.getpubkey();
+        String privkey=obj.getprivkey();
+        String pk1="",str="",str2="";
+        try 
+            {
+                pk1=din.readUTF();  
+                System.out.println("Adam's public key is: "+pk1);   
+                dout.writeUTF(pubkey);  
+                dout.flush();  
+            } 
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }  
+        while(true)
+        {
+            int flag=0;
+            try 
+            {
+                str=din.readUTF(); 
+                String msg=obj.decrypt(str,privkey);
+                System.out.println(str);
+                System.out.println("Adam says: "+msg);  
+                str2=br.readLine();
+                if(msg.equals("stop"))
+                    flag=1; 
+                String encryptedString = Base64.getEncoder().encodeToString(obj.encrypt(str2, pk1)); 
+                dout.writeUTF(encryptedString);  
+                dout.flush();  
+            } 
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            } 
+
+            if(flag==1)
+                break;
+        }
+    try{
+        din.close(); 
+        dout.close();   
+    }
+    catch(Exception e)
+    {
+        e.printStackTrace();
+    }
+    }
+} 
